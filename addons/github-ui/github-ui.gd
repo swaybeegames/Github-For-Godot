f96f3acd2a046fd12f616f2d0f4c86d9d649578b
Swaybee <swaybeegames@gmail.com>
2025-11-21 15:20:48 +0100

    Initial commit.

---
+++ addons/github-ui/github-ui.gd
@@ -0,0 +1,24 @@
+@tool
+extends EditorPlugin
+
+
+# A class member to hold the dock during the plugin life cycle.
+var dock: Control
+var dockActions: GitHub
+
+
+func _enter_tree():
+	dock = preload("res://addons/github-ui/scenes/github-pannel.tscn").instantiate()
+	dockActions = dock.get_node("GitHub")
+	if not dockActions.check_git():
+		dockActions.init()
+		print("A repository has been created.")
+	else:
+		print("A repository exists.")
+	add_control_to_dock(DOCK_SLOT_LEFT_BR, dock)
+
+
+func _exit_tree():
+	remove_control_from_docks(dock)
+	# Erase the control from the memory.
+	dock.free()
---
+++ addons/github-ui/scripts/github-actions.gd
@@ -0,0 +1,64 @@
+@tool
+class_name GitHub
+extends Node
+
+@onready var commitMessageNode = $PannelContainer/InfosContainer/CommitMessageContainer/CommitMessage
+
+func git(args: Array, ou: Array):
+	return OS.execute("git", args, ou)
+
+func tree():
+	var output = []
+	var err = git(["status", "--porcelain"], output)
+	print("output :")
+	print(output)
+	print("errors :")
+	print(err)
+
+func commit(message: String, output):
+	git(["add", "."], output)
+	var err = git(["commit", "-m", message], output)
+	if err:
+		print("Commit has not succeed.")
+	else:
+		print("Commit has been done successfully.")
+
+func pull():
+	pass
+
+func push():
+	pass
+
+func link(addr: String):
+	var output = []
+	var err = git(["remote", "add", "origin", addr], output)
+	if err:
+		print("Could not link to the remote repository.")
+
+func check_git():
+	var dir = DirAccess.open("res://")
+	if dir.dir_exists(".git"):
+		print("'.git' directory not found. Git repository is not initialized.")
+		return true
+	else:
+		print("'.git' directory found. Git repository is already initialized.")
+		return false
+
+func init():
+	var output = []
+	var err = git(["init"], output)
+	if err:
+		print("Repository could not be initialized.")
+	else:
+		print("Repository has been initialized.")
+		err = commit("Initial commit.", output)
+		if err:
+			print("The initial commit has not succeed.")
+		else:
+			print("Made the initial commit successfully")
+	
+
+func _on_commit_button_pressed() -> void:
+	var output = []
+	var message = commitMessageNode.text
+	commit(message, output)
